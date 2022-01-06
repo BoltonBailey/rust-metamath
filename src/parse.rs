006d0b6d8370bb39836f54ec28a620628472e3e1
= <jzw2@illinois.edu>
2022-01-05 19:16:53 -0600

    Added derive

--- src/parse.rs
+++ src/parse.rs
@@ -7,31 +7,6 @@
   combinator::map_res,
   sequence::tuple};
 
-#[derive(Debug,PartialEq)]
-pub struct Color {
-  pub red:     u8,
-  pub green:   u8,
-  pub blue:    u8,
-}
-
-//stuff with dollars
-#[derive(Debug,PartialEq)]
-pub enum Keyword {
-    LBrace,
-    RBrace,
-    C,
-    V,
-    F,
-    E,
-    D,
-    A,
-    P,
-    Dot,
-    LParen,
-    RParen,
-    LBracket,
-    RBracket,
-}
 
 // taken from Appendix E of the metamath language manual
 //
@@ -46,16 +21,20 @@ type TypeCode = String;
 type Label = String;
 type CompressedProofBlock = String;
 
+#[derive(Debug,PartialEq)]
 pub struct NonEmptyVec<T>(T, Vec<T>);
 
+#[derive(Debug,PartialEq)]
 pub struct Database(Vec<OutermostScopeStatement>);
 
+#[derive(Debug,PartialEq)]
 pub enum OutermostScopeStatement {
     IncludeStatement(String),
     ConstantStatement(NonEmptyVec<Constant>), // since it cannot be empty
     Statement(Statement),
 }
 
+#[derive(Debug,PartialEq)]
 pub enum Statement {
     Block(Vec<Statement>),
     VariableStatement(NonEmptyVec<Variable>),
@@ -65,17 +44,20 @@ pub enum Statement {
 }
 
 
+#[derive(Debug,PartialEq)]
 pub enum HypothesisStatement {
     FloatingStatement(Label, TypeCode, Variable),
     EssentialStatement(Label, TypeCode, Vec<MathSymbol>),
 }
 
 
+#[derive(Debug,PartialEq)]
 pub enum AssertStatement {
     AxiomStatement(Label, TypeCode, Vec<MathSymbol>),
     ProvableStatement(Label, TypeCode, Vec<MathSymbol>, Proof),
 }
 
+#[derive(Debug,PartialEq)]
 pub enum Proof {
     UncompressedProof(NonEmptyVec<Option<Label>>), // the option represents whether it is "?"
     CompressedProof(Vec<Label>, NonEmptyVec<CompressedProofBlock>),
