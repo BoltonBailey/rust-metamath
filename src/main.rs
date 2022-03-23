7724ebaff03a11b6e39755b5ae75bfb1c51ac690
John Wang <jzw2@illinois.edu>
2022-03-22 20:16:44 -0500

    Add stupid file

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 mod framestack;
 mod reader;
 mod database;
+mod stupid;
 use std::fs::File;
 use std::io::BufReader;
 
 use crate::database::MM;
 use crate::reader::Tokens;
+use crate::stupid::test;
 
 // first one is label type,
 
@@ -16,15 +18,21 @@ fn main() {
 
     // println!("Got cmd argumnets {:?}", args);
 
-    let mut mm = MM::new(args.get(2).cloned(), args.get(3).cloned());
+    let is_stupid = args.get(2).map(|x| x == "-stupid").is_some();
+    if is_stupid {
+        test();
+    } else {
+        let mut mm = MM::new(args.get(2).cloned(), args.get(3).cloned());
 
-    let file = File::open(args[1].clone()).expect("Failed to find file");
+        let file = File::open(args[1].clone()).expect("Failed to find file");
+
     // println!("Found file name {:?}", args[1]);
-    use std::time::Instant;
-    let now = Instant::now();
+        use std::time::Instant;
+        let now = Instant::now();
 
-    mm.read(&mut Tokens::new(BufReader::new(file)));
-    mm.dump();
-    let elapsed = now.elapsed();
-    println!("Finished checking in {:.2?}", elapsed);
+        mm.read(&mut Tokens::new(BufReader::new(file)));
+        mm.dump();
+        let elapsed = now.elapsed();
+        println!("Finished checking in {:.2?}", elapsed);
+    }
 }
